c6699bba4dd17e6b3440e427c799a0dc6288533f
Michael Noronha <michaeltnoronha@gmail.com>
2017-12-20 20:56:53 -0500

    Add remaining tests on node types

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-run:
-	python3 regular.py
-
 test:
 	python3 tests.py
+
+run:
+	python3 regular.py
--- tests.py
+++ tests.py
@@ -17,8 +17,85 @@ class TestNodes(unittest.TestCase):
         self.assertTrue(isinstance(last.derive("d"), NeverMatches))
 
         self.assertTrue(isinstance(middle.derive("b"), CharacterNode))
+        self.assertTrue(isinstance(middle.derive("c"), NeverMatches))
         self.assertEqual(middle.derive("b"), last)
 
+        self.assertTrue(isinstance(first.derive("a"), CharacterNode))
+        self.assertTrue(isinstance(first.derive("b"), NeverMatches))
+        self.assertEqual(first.derive("a"), middle)
+
+
+    def test_list_with_alternation(self):
+        last = CharacterNode("d", EmptyString())
+        middle = AlternationNode([CharacterNode("b", last), CharacterNode("c", last)])
+        first = CharacterNode("a", middle)
+
+        self.assertTrue(isinstance(last.derive("d"), EmptyString))
+        self.assertTrue(isinstance(last.derive("c"), NeverMatches))
+
+        self.assertTrue(isinstance(middle.derive("b"), CharacterNode))
+        self.assertTrue(isinstance(middle.derive("c"), CharacterNode))
+        self.assertTrue(isinstance(middle.derive("a"), NeverMatches))
+        self.assertEqual(middle.derive("b"), last)
+        self.assertEqual(middle.derive("c"), last)
+
+        self.assertTrue(isinstance(first.derive("a"), AlternationNode))
+        self.assertTrue(isinstance(first.derive("b"), NeverMatches))
+        self.assertEqual(first.derive("a"), middle)
+
+    def test_multiple_alternations(self):
+        last = CharacterNode("d", EmptyString())
+        middle2 = AlternationNode([CharacterNode("b", last), CharacterNode("c", last)])
+        middle1 = AlternationNode([CharacterNode("b", middle2), CharacterNode("c", middle2)])
+        first = CharacterNode("a", middle1)
+
+        self.assertTrue(last.canMatchMore())
+        self.assertTrue(isinstance(last.derive("d"), EmptyString))
+        self.assertTrue(isinstance(last.derive("c"), NeverMatches))
+
+        self.assertTrue(middle2.canMatchMore())
+        self.assertTrue(isinstance(middle2.derive("b"), CharacterNode))
+        self.assertTrue(isinstance(middle2.derive("c"), CharacterNode))
+        self.assertTrue(isinstance(middle2.derive("a"), NeverMatches))
+        self.assertEqual(middle2.derive("b"), last)
+        self.assertEqual(middle2.derive("c"), last)
+
+        self.assertTrue(middle1.canMatchMore())
+        self.assertTrue(isinstance(middle1.derive("b"), AlternationNode))
+        self.assertTrue(isinstance(middle1.derive("c"), AlternationNode))
+        self.assertTrue(isinstance(middle1.derive("a"), NeverMatches))
+        self.assertEqual(middle1.derive("b"), middle2)
+        self.assertEqual(middle1.derive("c"), middle2)
+
+        self.assertTrue(first.canMatchMore())
+        self.assertTrue(isinstance(first.derive("a"), AlternationNode))
+        self.assertTrue(isinstance(first.derive("b"), NeverMatches))
+        self.assertEqual(first.derive("a"), middle1)
+
+
+    def test_any_character_node(self):
+
+        any_node = AnyCharacterNode(EmptyString())
+
+        self.assertTrue(isinstance(any_node.derive("a"), EmptyString))
+        self.assertTrue(isinstance(any_node.derive("b"), EmptyString))
+
+    def test_repetition_node(self):
+
+        tail = CharacterNode("d", EmptyString())
+        repetition = RepetitionNode(tail)
+        repetition_body = CharacterNode("a", CharacterNode("b", CharacterNode(
+            "c", repetition)))
+        repetition.head = repetition_body
+
+        self.assertTrue(isinstance(repetition.derive("a"), CharacterNode))
+        self.assertTrue(isinstance(repetition.derive("d"), EmptyString))
+        self.assertTrue(isinstance(repetition.derive("a").derive("b").derive("c"),
+                                   RepetitionNode))
+
+        self.assertEqual(repetition.derive("a").derive("b").derive("c"), repetition)
+        self.assertEqual(repetition.derive("a").derive("b").derive("c").derive("a"),
+                         repetition.derive("a"))
 
 
 class TestCompiler(unittest.TestCase):
